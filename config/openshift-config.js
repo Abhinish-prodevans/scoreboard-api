fc8025705d263047a767249a4a7ee1de6e2f854a
Abhinish <abhinish@prodevans.com>
2018-03-03 15:23:00 +0530

    mondb_url changed2

--- config/openshift-config.js
+++ config/openshift-config.js
@@ -1,6 +1,6 @@
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL || 'mongodb://admin:JwufJRgf5MMnO3jB@172.30.237.235:27017/ScoreBoard',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
