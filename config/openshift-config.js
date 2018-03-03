34862e3aa3f5fd697690c1f70b911ab1fe0dbd59
Abhinish <abhinish@prodevans.com>
2018-03-03 16:38:35 +0530

    mondb_url changed4

--- config/openshift-config.js
+++ config/openshift-config.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
-if (mongoURL === undefined && process.env.DATABASE_SERVICE_NAME) {
+if (typeof mongoURL === "undefined" && process.env.DATABASE_SERVICE_NAME) {
     var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
         mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
         mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
